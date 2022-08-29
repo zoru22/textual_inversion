aa82be390c968c5d180d7df7f6263f15ede5d483
zoru22 <111854061+zoru22@users.noreply.github.com>
2022-08-28 22:00:01 -0600

    sliding in the merge_embeddings changes

--- merge_embeddings.py
+++ merge_embeddings.py
@@ -1,15 +1,14 @@
-from ldm.modules.encoders.modules import BERTTokenizer
+from ldm.modules.encoders.modules import BERTTokenizer, FrozenCLIPEmbedder
 from ldm.modules.embedding_manager import EmbeddingManager
 
-import argparse, os
+import argparse
 from functools import partial
 
 import torch
 
 def get_placeholder_loop(placeholder_string, tokenizer):
-    
     new_placeholder   = None
-    
+
     while True:
         if new_placeholder is None:
             new_placeholder = input(f"Placeholder string {placeholder_string} was already used. Please enter a replacement string: ")
@@ -21,14 +20,21 @@ def get_placeholder_loop(placeholder_string, tokenizer):
         if torch.count_nonzero(token) == 3:
             return new_placeholder, token[0, 1]
 
-if __name__ == "__main__":
-
+def get_opts():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--manager_ckpts", 
-        type=str, 
-        nargs="+", 
+        "--tokenizer",
+        type=str,
+        required=True,
+        choices=['CLIP', 'BERT'],
+        default='BERT'
+    )
+
+    parser.add_argument(
+        "--manager_ckpts",
+        type=str,
+        nargs="+",
         required=True,
         help="Paths to a set of embedding managers to be merged."
     )
@@ -40,20 +46,31 @@ if __name__ == "__main__":
         help="Output path for the merged manager",
     )
 
-    args = parser.parse_args()
+    opts = parser.parse_args()
 
-    tokenizer = BERTTokenizer(vq_interface=False, max_length=77)
-    EmbeddingManager = partial(EmbeddingManager, tokenizer, ["*"])
+    if opts.tokenizer == 'BERT':
+        opts.tokenizer = BERTTokenizer(vq_interface=False, max_length=77)
+    elif opts.tokenizer == 'CLIP':
+        opts.tokenizer = FrozenCLIPEmbedder()
+    else:
+        raise Exception(f'Unrecognized --tokenizer selection.')
 
-    string_to_token_dict = {}    
+    return opts
+
+def main():
+    opts = get_opts()
+
+    manager = partial(EmbeddingManager, opts.tokenizer, ["*"])
+
+    string_to_token_dict = {}
     string_to_param_dict = torch.nn.ParameterDict()
 
     placeholder_to_src = {}
 
-    for manager_ckpt in args.manager_ckpts:
+    for manager_ckpt in opts.manager_ckpts:
         print(f"Parsing {manager_ckpt}...")
 
-        manager = EmbeddingManager()
+        manager = manager()
         manager.load(manager_ckpt)
 
         for placeholder_string in manager.string_to_token_dict:
@@ -63,21 +80,20 @@ if __name__ == "__main__":
 
                 placeholder_to_src[placeholder_string] = manager_ckpt
             else:
-                new_placeholder, new_token = get_placeholder_loop(placeholder_string, tokenizer)
+                new_placeholder, new_token = get_placeholder_loop(placeholder_string, opts.tokenizer)
                 string_to_token_dict[new_placeholder] = new_token
                 string_to_param_dict[new_placeholder] = manager.string_to_param_dict[placeholder_string]
 
                 placeholder_to_src[new_placeholder] = manager_ckpt
 
     print("Saving combined manager...")
-    merged_manager = EmbeddingManager()
+    merged_manager = manager()
     merged_manager.string_to_param_dict = string_to_param_dict
     merged_manager.string_to_token_dict = string_to_token_dict
-    merged_manager.save(args.output_path)
+    merged_manager.save(opts.output_path)
 
     print("Managers merged. Final list of placeholders: ")
     print(placeholder_to_src)
 
-
-
-                
+if __name__ == "__main__":
+    main()
